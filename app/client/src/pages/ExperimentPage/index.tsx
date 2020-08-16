0a42ab3fb0dd2ed8879efeae3ab64b1af4583f42
ishangupta-ds <ishan.gupta@mayadata.io>
2020-08-16 10:40:38 +0530

    formatting updated according to prettier rc.
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

--- app/client/src/pages/ExperimentPage/index.tsx
+++ app/client/src/pages/ExperimentPage/index.tsx
@@ -1,64 +1,97 @@
-import * as React from 'react';
-import { useSelector } from 'react-redux';
-import { BackButton, ExperimentInfo, InstallChaos, UsefulLinks, ExperimentHeader } from '../../components';
-import Footer from '../../components/Footer';
-import { history } from '../../redux/configureStore';
-import { ExperimentGroup, Link } from '../../redux/model';
-import { RootState } from '../../redux/reducers';
-import { getExpRunCount } from '../../utils';
-import { useStyles } from './styles';
-import MainHeader from '../../components/Header';
-import DeveloperGuide from '../../components/DeveloperGuide';
-import '../../scrollbar.css';
+import * as React from "react";
+import { useSelector } from "react-redux";
+import {
+	BackButton,
+	ExperimentInfo,
+	InstallChaos,
+	UsefulLinks,
+	ExperimentHeader,
+} from "../../components";
+import Footer from "../../components/Footer";
+import { history } from "../../redux/configureStore";
+import { ExperimentGroup, Link } from "../../redux/model";
+import { RootState } from "../../redux/reducers";
+import { getExpRunCount } from "../../utils";
+import { useStyles } from "./styles";
+import MainHeader from "../../components/Header";
+import DeveloperGuide from "../../components/DeveloperGuide";
+import "../../scrollbar.css";
 
 const getIconUrl = (path: any, version: string) => {
-	let baseURL: string = '';
-	if (process.env.NODE_ENV.trim() === 'development' || process.env.NODE_ENV.trim() === 'test') {
+	let baseURL: string = "";
+	if (
+		process.env.NODE_ENV.trim() === "development" ||
+		process.env.NODE_ENV.trim() === "test"
+	) {
 		baseURL = `${window.location.protocol}//${window.location.hostname}:8080`;
-	} else baseURL = '/api';
-	if (path[2] === 'all-experiments') return baseURL + '/icon/' + version + '/' + path[1] + '/' + path[1] + '.png';
-	return baseURL + '/icon/' + version + '/' + path[1] + '/' + path[2] + '.png';
+	} else baseURL = "/api";
+	if (path[2] === "all-experiments")
+		return (
+			baseURL +
+			"/icon/" +
+			version +
+			"/" +
+			path[1] +
+			"/" +
+			path[1] +
+			".png"
+		);
+	return (
+		baseURL + "/icon/" + version + "/" + path[1] + "/" + path[2] + ".png"
+	);
 };
 
 function ExperimentPage(props: any) {
 	const classes = useStyles();
 	const match = props.match;
-	const path = match.url.split('/');
+	const path = match.url.split("/");
 	const chartGroupId: string = match.params.chartGroupId;
 	const chartId: string = match.params.chartId;
-	const { chartData, analyticsData, versionData } = useSelector((state: RootState) => state);
-	const chartGroup: ExperimentGroup = chartData.allExperimentGroups.filter((g) => g.metadataName === chartGroupId)[0];
+	const { chartData, analyticsData, versionData } = useSelector(
+		(state: RootState) => state
+	);
+	const chartGroup: ExperimentGroup = chartData.allExperimentGroups.filter(
+		(g) => g.metadataName === chartGroupId
+	)[0];
 	const chart: any =
-		chartId === 'all-experiments'
+		chartId === "all-experiments"
 			? chartGroup
-			: chartGroup && chartGroup.experiments.filter((e) => e.metadataName === chartId)[0];
+			: chartGroup &&
+			  chartGroup.experiments.filter(
+					(e) => e.metadataName === chartId
+			  )[0];
 
 	React.useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
 
 	if (!chartGroup || !chart) {
-		history.push('/');
+		history.push("/");
 		return <div />;
 	} else {
-		const hubUrl: string = `https://hub.litmuschaos.io/api/chaos/${versionData.currentVersion}?file=${chart.chaosExpCRDLink.split(
-			'/chaos-charts/master/'
-		)[1]}`;
-		let rbacUrl: string = '',
-			engineUrl: string = '',
+		const hubUrl: string = `https://hub.litmuschaos.io/api/chaos/${
+			versionData.currentVersion
+		}?file=${chart.chaosExpCRDLink.split("/chaos-charts/master/")[1]}`;
+		let rbacUrl: string = "",
+			engineUrl: string = "",
 			expCount: number = 0,
-			videoURL: string = '';
+			videoURL: string = "";
 		if (JSON.stringify(chartGroup) !== JSON.stringify(chart)) {
-			const url: string[] = hubUrl.split('/');
-			url[url.length - 1] = 'rbac.yaml';
-			rbacUrl = url.join('/');
-			url[url.length - 1] = 'engine.yaml';
-			engineUrl = url.join('/');
+			const url: string[] = hubUrl.split("/");
+			url[url.length - 1] = "rbac.yaml";
+			rbacUrl = url.join("/");
+			url[url.length - 1] = "engine.yaml";
+			engineUrl = url.join("/");
 			expCount = getExpRunCount(chart, analyticsData.expAnalytics);
-			const video: any = chart.links.filter((l: Link) => l.name === 'Video')[0];
-			videoURL = video ? video.url : '';
+			const video: any = chart.links.filter(
+				(l: Link) => l.name === "Video"
+			)[0];
+			videoURL = video ? video.url : "";
 		} else {
-			expCount = getExpRunCount(chart.experiments, analyticsData.expAnalytics);
+			expCount = getExpRunCount(
+				chart.experiments,
+				analyticsData.expAnalytics
+			);
 		}
 
 		return (
@@ -77,7 +110,10 @@ function ExperimentPage(props: any) {
 											title={chart.name}
 											description={chart.description}
 											runCount={expCount}
-											urlToIcon={getIconUrl(path, versionData.currentVersion)}
+											urlToIcon={getIconUrl(
+												path,
+												versionData.currentVersion
+											)}
 										/>
 									</div>
 								</div>
@@ -91,24 +127,41 @@ function ExperimentPage(props: any) {
 							<div className={classes.detailDiv}>
 								<div className={classes.expInfo}>
 									<div className={classes.expInfoDiv}>
-										<ExperimentInfo description={chart.description} videoURL={videoURL} />
+										<ExperimentInfo
+											description={chart.description}
+											videoURL={videoURL}
+										/>
 										<div
 											style={{
-												marginTop: 'auto',
-												marginBottom: 40
+												marginTop: "auto",
+												marginBottom: 40,
 											}}
 										>
-											<hr className={classes.horizontalLine} />
+											<hr
+												className={
+													classes.horizontalLine
+												}
+											/>
 											<div>
-												<div className={classes.note}>PRE-REQUISITE:</div>
+												<div className={classes.note}>
+													PRE-REQUISITE:
+												</div>
 												<div>
-													<a href="https://docs.litmuschaos.io/docs/getstarted/" target="_">
+													<a
+														href="https://docs.litmuschaos.io/docs/getstarted/"
+														target="_"
+													>
 														Install Litmus Operator
 													</a>
-													: a tool for injecting Chaos Experiments
+													: a tool for injecting Chaos
+													Experiments
 												</div>
 											</div>
-											<hr className={classes.horizontalLine} />
+											<hr
+												className={
+													classes.horizontalLine
+												}
+											/>
 										</div>
 									</div>
 									{/* Useful Links Section */}
